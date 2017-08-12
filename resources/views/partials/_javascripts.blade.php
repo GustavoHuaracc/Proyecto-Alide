0cfb375cbcee29320b83cc6af766f35ecffd88a9
Unknown <gustavo.huaracc@unmsm.edu.pe>
2017-08-11 21:48:44 -0500

    avance de maquetas

---
+++ resources/views/partials/_javascripts.blade.php
@@ -0,0 +1,43 @@
+<!-- Javascript Libs -->
+{{-- {!!Html::script('../vendor/tcg/voyager/publishable/assets/lib/js/bootstrap.min.js')!!}
+
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/lib/js/bootstrap-switch.min.js')!!}
+
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/lib/js/jquery.matchHeight-min.js')!!}
+
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/lib/js/jquery.dataTables.min.js')!!}
+
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/lib/js/dataTables.bootstrap.min.js')!!}
+
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/lib/js/toastr.min.js')!!}
+
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/lib/js/perfect-scrollbar.jquery.min.js')!!}
+
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/js/select2/select2.min.js')!!}
+
+
+
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/js/jquery.cookie.js')!!}
+
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/js/moment-with-locales.min.js')!!}
+
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/js/datetimepicker/bootstrap-datetimepicker.min.js')!!} --}}
+<!-- Javascript -->
+
+
+{{-- 
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/lib/js/bootstrap-switch.min.js')!!}
+
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/js/bootstrap-toggle.min.js')!!}
+
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/js/readmore.min.js')!!} --}}
+
+{{-- {!!Html::script('../vendor/tcg/voyager/publishable/assets/js/val.js')!!}
+
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/js/app.js')!!}
+
+{!!Html::script('../vendor/tcg/voyager/publishable/assets/js/helpers.js')!!} --}}
+{!!Html::script('https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js')!!}
+{!!Html::script('../js/script-notificacion.js')!!}
+  <script src=""></script>
+
